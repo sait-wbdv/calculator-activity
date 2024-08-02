70336ff374ba9060a3d8280bd5683b5c53c10eb7
Ashlyn Knox <ashlyn@lilyx.io>
2024-08-02 12:50:23 -0600

    function setup with completed conditional logic

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 "use strict";
 
-// calculation values
+// calculation values -- they do NOT need to be the same as the values in your function
 const num1 = 8;
 const num2 = 33;
 const operator = "+";
 
-const add = num1 + num2;
-
-const subtract = num1 - num2;
-
-const divide = num1 / num2;
-
-const multiply = num1 * num2;
-
-// add the your calculations to a conditional statement
-
-if (/* operator is equal to + */) {
-  // return the addition equation 
-} else if (/* operator is equal to - */) {
-  // return the subtraction equation
-} else if (/* operator is equal to / */) {
-  // return the division equation
-} else if (/* operator is equal to * */) {
-  // return the multiplication equation
+// wrap this in a function that accepts num1, num2, and operator as arguments
+// remove the console.log() as they won't be needed in here anymore
+let result;
+if (operator === "+") {
+  result = num1 + num2;
+  console.log(result);
+  return result;
+} else if (operator === "-") {
+  result = num1 - num2;
+  console.log(result);
+  return result;
+} else if (operator === "/") {
+  result = num1 / num2;
+  console.log(result);
+  return result;
+} else if (operator === "*") {
+  result = num1 * num2;
+  console.log(result);
+  return result;
 } else {
-  console.error("the operator must be either + - / or * ")
-  return 0;
+  console.error("the operator must be either + - / or * ");
+  return;
 }
+
+// invoke the function and the variables into it
