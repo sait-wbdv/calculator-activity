385333a5076567ddf6ef35fe5e505002955fc392
Ashlyn Knox <ashlyn@lilyx.io>
2024-08-02 12:55:17 -0600

    completed code with cli input notes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// process.argv is a way to take command line arguments in node.js you would use a different approach for the browser
+
 const args = process.argv.slice(2);
 
 if (args.length !== 3) {
@@ -7,6 +9,30 @@ if (args.length !== 3) {
   process.exit(1);
 }
 
+// this takes the values from the command line input and assigns them to variables
 const [num1, operator, num2] = args;
 
-console.log(num1, operator, num2);
+function calculate(num1, operator, num2) {
+  let result;
+  if (operator === "+") {
+    result = num1 + num2;
+    return result;
+  } else if (operator === "-") {
+    result = num1 - num2;
+    return result;
+  } else if (operator === "/") {
+    result = num1 / num2;
+    return result;
+  } else if (operator === "*") {
+    result = num1 * num2;
+    return result;
+  } else {
+    console.error("the operator must be either + - / or * ");
+    return;
+  }
+}
+
+const result = calculate(num1, operator, num2);
+
+// logging the result is just to show that it works in the command line
+console.log(result);
