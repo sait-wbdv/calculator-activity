ecf96e2d7e467d131e786a00c6e87fdc3958c76b
Ashlyn Knox <ashlyn@lilyx.io>
2024-08-02 12:40:41 -0600

    conditional logic assisted code template

---
+++ app.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// calculation values
+const num1 = 8;
+const num2 = 33;
+const operator = "+";
+
+const add = num1 + num2;
+
+const subtract = num1 - num2;
+
+const divide = num1 / num2;
+
+const multiply = num1 * num2;
+
+// add the your calculations to a conditional statement
+
+if (/* operator is equal to + */) {
+  // return the addition equation 
+} else if (/* operator is equal to - */) {
+  // return the subtraction equation
+} else if (/* operator is equal to / */) {
+  // return the division equation
+} else if (/* operator is equal to * */) {
+  // return the multiplication equation
+} else {
+  console.error("the operator must be either + - / or * ")
+  return 0;
+}
