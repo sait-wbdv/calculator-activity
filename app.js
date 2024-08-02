f8c295e0a8b340df6c736c7dd363bc3f6a449f4d
Ashlyn Knox <ashlyn@lilyx.io>
2024-08-02 12:57:51 -0600

    variable instructions added to comments

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// create variables for the 2 values to be calculated
+
+// create variables that equal the calculations for + - / and *
+
+// log each of the different calculations so that it's clear that they all work as expected
