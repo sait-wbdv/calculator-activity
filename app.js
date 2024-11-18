c424c2c6e0de37d3330c7bb98642385a8bdac8d3
Ashlyn Knox <ashlyn@lilyx.io>
2024-11-18 15:55:30 -0700

    refactor and better error handling

--- app.js
+++ app.js
@@ -4,35 +4,39 @@
 
 const args = process.argv.slice(2);
 
+const add = require("./modules/add");
+const subtract = require("./modules/subtract");
+const divide = require("./modules/divide");
+const multiply = require("./modules/multiply");
+
+if (args.length > 3) {
+  console.error(`Too many arguments: ${args.length} \nArgs: ${args}`);
+  process.exit(1);
+}
 if (args.length !== 3) {
   console.error("Usage: node app.js <num1> <operator> <num2>");
   process.exit(1);
 }
-
 // this takes the values from the command line input and assigns them to variables
 const [num1, operator, num2] = args;
 
 function calculate(num1, operator, num2) {
-  let result;
-  if (operator === "+") {
-    result = num1 + num2;
-    return result;
-  } else if (operator === "-") {
-    result = num1 - num2;
-    return result;
-  } else if (operator === "/") {
-    result = num1 / num2;
-    return result;
-  } else if (operator === "*") {
-    result = num1 * num2;
-    return result;
-  } else {
-    console.error("the operator must be either + - / or * ");
-    return;
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    default:
+      console.error("the operator must be either + - / or * ");
+      return;
   }
 }
 
-const result = calculate(num1, operator, num2);
+const result = calculate(parseFloat(num1), operator, parseFloat(num2));
 
 // logging the result is just to show that it works in the command line
 console.log(result);
